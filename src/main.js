5cd9672db2dc99d6e36388ab9751594c08cec714
shuidajie <tanghui0926@163.com>
2022-10-26 10:02:51 +0800

    '项目初始化-调整项目目录结构‘

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+/* 加载全局样式 */
+import './style/index.less'
 createApp(App).use(store).use(router).mount('#app')
